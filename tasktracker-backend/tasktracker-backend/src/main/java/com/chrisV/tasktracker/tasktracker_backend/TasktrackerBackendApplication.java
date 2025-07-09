a3a2eed4536fb4681806d80e08c2a5594986811a
christopher <christophervalle3338@gmail.com>
2025-07-09 11:16:56 -0700

    Create SPRING-BOOT project

---
+++ tasktracker-backend/tasktracker-backend/src/main/java/com/chrisV/tasktracker/tasktracker_backend/TasktrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chrisV.tasktracker.tasktracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktrackerBackendApplication.class, args);
+	}
+
+}
