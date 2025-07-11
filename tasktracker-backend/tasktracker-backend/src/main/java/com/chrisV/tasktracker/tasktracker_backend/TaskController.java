9e0e5e9008faec5113fe7ecb41e995096584a917
christopher <christophervalle3338@gmail.com>
2025-07-11 10:59:25 -0700

    Create task REPO & Controller using JPArepository

---
+++ tasktracker-backend/tasktracker-backend/src/main/java/com/chrisV/tasktracker/tasktracker_backend/TaskController.java
@@ -0,0 +1,38 @@
+package com.chrisV.tasktracker.tasktracker_backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*") //allow front-end access
+public class TaskController {
+
+    @Autowired
+    private TaskRepository repo;
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return repo.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task data) {
+        Task task = repo.findById(id).orElseThrow();
+        task.setTitle(data.getTiltle());
+        task.setCompleted(data.isCompleted());
+        return repo.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        repo.deleteById(id);
+    }
+}
