962412b391f83cff233c2087380966222c144b9d
christopher <christophervalle3338@gmail.com>
2025-07-13 22:03:27 -0700

    Config MYSQL DB connection

--- tasktracker-backend/src/main/java/com/chrisV/tasktracker/backend/TasktrackerBackendApplication.java
+++ tasktracker-backend/src/main/java/com/chrisV/tasktracker/backend/TasktrackerBackendApplication.java
@@ -9,5 +9,4 @@ public class TasktrackerBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasktrackerBackendApplication.class, args);
 	}
-
 }
--- tasktracker-backend/src/test/java/com/chrisV/tasktracker/tasktracker_backend/TasktrackerBackendApplicationTests.java
+++ tasktracker-backend/src/test/java/com/chrisV/tasktracker/tasktracker_backend/TasktrackerBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.chrisV.tasktracker.tasktracker_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes =TasktrackerBackendApplicationTests.class)
 class TasktrackerBackendApplicationTests {
 
 	@Test
