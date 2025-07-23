69aecd118cd954da383d8b75dc7cc492242048c3
christopher <christophervalle3338@gmail.com>
2025-07-22 17:12:08 -0700

    Reinstall frontend Vite project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/tasks")
-    .then(res => res.json())
-    .then(data => setTasks(data))
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
+    <>
       <div>
-        <h1>Task Tracker</h1>
-        <p>Starting to build frontend for tasktracker</p>
-        <ul>
-          {tasks.map(task => (
-            <li key={task.id}>{task.title}</li>
-          ))}
-        </ul>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
